f3c5537ef84bb40921c5ddeab6360415dac791a0
Aman Ambastha <aambastha@ucsd.edu>
2025-11-09 17:00:07 -0800

    Fixed constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
 //Stage Dimensions
 export const STAGE_WIDTH = 800;
 export const STAGE_HEIGHT = 600;
-=======
-//Stage Dimensions
-export const STAGE_WIDTH = 800;
-export const STAGE_HEIGHT = 600;
-
-//
->>>>>>> 8e87ebdd4660d07fee2a89ede5b9eb0f198123c6
