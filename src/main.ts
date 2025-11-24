585c87340d2ac79f12a3338fec6470860ddb37a0
naomimetzler <nmetzler@ucsd.edu>
2025-11-23 19:08:54 -0800

    removed stage dimension imports from main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { AppScreenSwitcher } from "./controller/AppScreenSwitcher";
 import { ActModels } from "./models/GenericActModel";
-import { STAGE_WIDTH, STAGE_HEIGHT } from "./constants";
 
 function main() {
     const switcher = new AppScreenSwitcher(ActModels.getInstance().getQuestions());
